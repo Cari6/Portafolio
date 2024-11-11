f8851c4ae63f42327ec61021cabf471d379ea9a1
Cari6 <carolinasandovalg6@gmail.com>
2024-11-10 21:29:07 -0300

    linkedin icon

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -13,7 +13,7 @@ export const email = "carolinasandovalg6@gmail.com";
 export const contactInfo = [
   {
     href: "https://www.linkedin.com/in/carolina-sandoval-678656333/",
-    src: "/skills/linkedin.png",
+    src: "/skills/linkedin.jpg",
     alt: "LinkedIn",
     id: "linkedin",
   },
